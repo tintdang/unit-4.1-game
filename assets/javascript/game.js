99d3d99d55e40da56965ff010f02cd90717048b4
TDang <tin2dang@gmail.com>
2018-07-30 10:11:05 -0500

    added additional comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 // make my character variables
 var saber = {
-    healthPoints: ((Math.floor(Math.random() * 9) * 10)+ 100), //100,110... 180;
+    healthPoints: ((Math.floor(Math.random() * 9)   )+ 100), //100,110... 180;
     attackPower: ((Math.floor(Math.random()* 6)+ 5)),//5-10 attack power
     counterPower: ((Math.floor(Math.random() * 5) * 5) + 5), //counter attack power from 5-25 iterates from 5
 }
@@ -19,7 +19,22 @@ var archer = {
     counterPower: ((Math.floor(Math.random() * 5) * 5) + 5), //counter attack power from 5-25 iterates from 5
 }
 
+var character =[];
+
 // call stats ie by lancer.healthPoints
+
+//Add to the array?
+character.push(saber, lancer, archer);
+
+//later can reset by clearing the list and readding with .append()
+
+// Create arrays for attackers and defenders
+
+//object move to another object then .hide();
+
+//transfer stas over 
+
+
 
 //Reset stats with restart button
 function reset() {
